20e46bd02c744c076b251693551ceb69b3b2dc89
ratihayu <ratih.ayu19@yahoo.com>
2015-06-29 12:55:58 +0700

    lagi komplain_v4.sql

--- application/views/komplain/add.php
+++ application/views/komplain/add.php
@@ -17,10 +17,115 @@
           <div class="row">
             <!-- left column -->
             <div class="col-md-6">
+              <!-- general form elements disabled -->
+              <div class="box box-warning">
+                <div class="box-header">
+                  <h3 class="box-title">Data Komplain</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <form role="form">
+
+                    <div class="form-group">
+                    <label>Tanggal Komplain:</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+                      <input type="date" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask/>
+                    </div><!-- /.input group -->
+                  </div><!-- /.form group -->
+                  <div class="form-group">
+                    <label>Tanggal Closed:</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+                      <input type="date" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask/>
+                    </div><!-- /.input group -->
+                  </div><!-- /.form group -->
+                  <div class="form-group">
+                    <label>Janji:</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-calendar"></i>
+                      </div>
+                      <input type="date" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask/>
+                    </div><!-- /.input group -->
+                  </div><!-- /.form group -->
+
+                  <!-- select -->
+                    <div class="form-group">
+                      <label>Media</label>
+                      <select class="form-control">
+                        <option>option 1</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                        <option>option 4</option>
+                        <option>option 5</option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                      <label>Layanan</label>
+                      <select class="form-control">
+                        <option>option 1</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                        <option>option 4</option>
+                        <option>option 5</option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                      <label>Jenis Komplain</label>
+                      <select class="form-control">
+                        <option>option 1</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                        <option>option 4</option>
+                        <option>option 5</option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                      <label>Status Komplain</label>
+                      <select class="form-control">
+                        <option>option 1</option>
+                        <option>option 2</option>
+                        <option>option 3</option>
+                        <option>option 4</option>
+                        <option>option 5</option>
+                      </select>
+                    </div>
+
+                    <!-- textarea -->
+                    <div class="form-group">
+                      <label>Keluhan</label>
+                      <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                    </div>
+                    <div class="form-group">
+                      <label>Solusi</label>
+                      <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                    </div>
+                    <div class="form-group">
+                      <label>Keterangan</label>
+                      <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
+                    </div>
+
+                <!-- form start -->
+                    <div class="form-group">
+                      <label for="exampleInputFile">Dokumen Pendukung</label>
+                      <input type="file" id="exampleInputFile">
+                      <p class="help-block">Unggah file pendukung</p>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                    
+
+                </form>
+            </div><!--/.col (left) -->
+            <!-- right column -->
+            <div class="col-md-6">
               <!-- general form elements -->
               <div class="box box-primary">
                 <div class="box-header">
-                  <h3 class="box-title">Unggah File Data Komplain</h3>
+                  <h3 class="box-title">Unggah Data Komplain</h3>
                 </div><!-- /.box-header -->
                 <!-- form start -->
                 <form role="form">
@@ -37,140 +142,7 @@
                   </div>
                 </form>
               </div><!-- /.box -->
-
-            </div><!--/.col (left) -->
-            <!-- right column -->
-            <div class="col-md-6">
-              <!-- general form elements disabled -->
-              <div class="box box-warning">
-                <div class="box-header">
-                  <h3 class="box-title">General Elements</h3>
-                </div><!-- /.box-header -->
-                <div class="box-body">
-                  <form role="form">
-                    <!-- text input -->
-                    <div class="form-group">
-                      <label>Text</label>
-                      <input type="text" class="form-control" placeholder="Enter ..."/>
-                    </div>
-                    <div class="form-group">
-                      <label>Text Disabled</label>
-                      <input type="text" class="form-control" placeholder="Enter ..." disabled/>
-                    </div>
-
-                    <!-- textarea -->
-                    <div class="form-group">
-                      <label>Textarea</label>
-                      <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                    </div>
-                    <div class="form-group">
-                      <label>Textarea Disabled</label>
-                      <textarea class="form-control" rows="3" placeholder="Enter ..." disabled></textarea>
-                    </div>
-
-                    <!-- input states -->
-                    <div class="form-group has-success">
-                      <label class="control-label" for="inputSuccess"><i class="fa fa-check"></i> Input with success</label>
-                      <input type="text" class="form-control" id="inputSuccess" placeholder="Enter ..."/>
-                    </div>
-                    <div class="form-group has-warning">
-                      <label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Input with warning</label>
-                      <input type="text" class="form-control" id="inputWarning" placeholder="Enter ..."/>
-                    </div>
-                    <div class="form-group has-error">
-                      <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> Input with error</label>
-                      <input type="text" class="form-control" id="inputError" placeholder="Enter ..."/>
-                    </div>
-
-                    <!-- checkbox -->
-                    <div class="form-group">
-                      <div class="checkbox">
-                        <label>
-                          <input type="checkbox"/>
-                          Checkbox 1
-                        </label>
-                      </div>
-
-                      <div class="checkbox">
-                        <label>
-                          <input type="checkbox"/>
-                          Checkbox 2
-                        </label>
-                      </div>
-
-                      <div class="checkbox">
-                        <label>
-                          <input type="checkbox" disabled/>
-                          Checkbox disabled
-                        </label>
-                      </div>
-                    </div>
-
-                    <!-- radio -->
-                    <div class="form-group">
-                      <div class="radio">
-                        <label>
-                          <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-                          Option one is this and that&mdash;be sure to include why it's great
-                        </label>
-                      </div>
-                      <div class="radio">
-                        <label>
-                          <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                          Option two can be something else and selecting it will deselect option one
-                        </label>
-                      </div>
-                      <div class="radio">
-                        <label>
-                          <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled/>
-                          Option three is disabled
-                        </label>
-                      </div>
-                    </div>
-
-                    <!-- select -->
-                    <div class="form-group">
-                      <label>Select</label>
-                      <select class="form-control">
-                        <option>option 1</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                      </select>
-                    </div>
-                    <div class="form-group">
-                      <label>Select Disabled</label>
-                      <select class="form-control" disabled>
-                        <option>option 1</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                      </select>
-                    </div>
-
-                    <!-- Select multiple-->
-                    <div class="form-group">
-                      <label>Select Multiple</label>
-                      <select multiple class="form-control">
-                        <option>option 1</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                      </select>
-                    </div>
-                    <div class="form-group">
-                      <label>Select Multiple Disabled</label>
-                      <select multiple class="form-control" disabled>
-                        <option>option 1</option>
-                        <option>option 2</option>
-                        <option>option 3</option>
-                        <option>option 4</option>
-                        <option>option 5</option>
-                      </select>
-                    </div>
+            </div><!--/.col (right) -->
 
                   </form>
                 </div><!-- /.box-body -->
