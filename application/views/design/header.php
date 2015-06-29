8b4478f2b575e8d627ef1077aeed7388cdfb9b80
Dinar Winia Mahandhira <dinar.mahandhira@gmail.com>
2015-06-29 10:25:19 +0700

    delete account info in header

--- application/views/design/header.php
+++ application/views/design/header.php
@@ -86,15 +86,6 @@
       <aside class="main-sidebar">
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar" >
-          <!-- Sidebar user panel -->
-          <div class="user-panel">
-            <div class="pull-left image">
-              <img src="<?php echo base_url() ?>assets/dist/img/user.png" class="img-circle" alt="User Image" />
-            </div>
-            <div class="pull-left info">
-              <p><?php echo $nama ?></p>
-            </div>
-          </div>
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
             <li class="header">MENU</li>
