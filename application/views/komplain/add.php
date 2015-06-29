cc3e7bc3d5fe10a27103d770728a0de6707fd09b
ratihayu <ratih.ayu19@yahoo.com>
2015-06-29 12:17:19 +0700

    komplain_v4.sql

--- application/views/komplain/add.php
+++ application/views/komplain/add.php
@@ -3,13 +3,12 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
           <h1>
-            General Form Elements
-            <small>Preview</small>
+            Komplain
+            <small>Tambah Komplain</small>
           </h1>
           <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li><a href="#">Forms</a></li>
-            <li class="active">General Elements</li>
+            <li><a href="#">Komplain</a></li>
+            <li class="active">Tambah Komplain</li>
           </ol>
         </section>
 
@@ -21,28 +20,15 @@
               <!-- general form elements -->
               <div class="box box-primary">
                 <div class="box-header">
-                  <h3 class="box-title">Quick Example</h3>
+                  <h3 class="box-title">Unggah File Data Komplain</h3>
                 </div><!-- /.box-header -->
                 <!-- form start -->
                 <form role="form">
                   <div class="box-body">
                     <div class="form-group">
-                      <label for="exampleInputEmail1">Email address</label>
-                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputPassword1">Password</label>
-                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="form-group">
                       <label for="exampleInputFile">File input</label>
                       <input type="file" id="exampleInputFile">
-                      <p class="help-block">Example block-level help text here.</p>
-                    </div>
-                    <div class="checkbox">
-                      <label>
-                        <input type="checkbox"> Check me out
-                      </label>
+                      <p class="help-block">Unggah file dengan format .csv</p>
                     </div>
                   </div><!-- /.box-body -->
 
