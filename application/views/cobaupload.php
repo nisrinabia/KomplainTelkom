7e0e878b58d7abea141f59247dd379b5a5250044
Dinar Winia Mahandhira <dinar.mahandhira@gmail.com>
2015-07-08 13:02:36 +0700

    benerin addkomplain by file, update db v13

--- application/views/cobaupload.php
+++ application/views/cobaupload.php
@@ -3,5 +3,8 @@ $date = strtotime($s);
 /*echo $date . ' ';
 echo convert(datetime,'18-06-12 10:34:09 PM',1) . ' ';
 echo date('d/m/Y H:i:s', $date);*/
-$a = 'closed';
-echo ($a == 'closed' ? 1 : 0);
+/*$a = 'closed';
+echo ($a == 'closed' ? 1 : 0);*/
+$a = '12:00';
+$a = $a + '12:00';
+echo $a;
